90248176867bd74e57ceb54d888089475e5d347c
Victor Campos <73249160+victorcamposr@users.noreply.github.com>
2023-04-22 17:25:23 -0300

    Add files via upload
    Refactoring improvements

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-//DOM
+//variables
 
 const screen1 = document.querySelector('.screen1')
 const screen2 = document.querySelector('.screen2')
 const errormessage = document.querySelector('.errormessage')
+const btnTry = document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
 let xAttempts = 1
 const randomNumber = Math.round(Math.random() * 10)
 
 //Events
-const btnTry = document.querySelector('#btnTry')
-const btnReset = document.querySelector('#btnReset')
 
+//Try reset application
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', resetKey)
 
 // console.log(randomNumber)
 //Show popups of Error and Sucess
@@ -22,21 +26,21 @@ function showPopup(type) {
     }, 1000)
 }
 
-
-// Click for shoot events
+// Click for shoot events call back
 function handleTryClick(event) {
     event.preventDefault() //Define que o evento nao vai ser padrao, nesse caso do formulario nao vai encaminhar ele.
     const inputNumber = document.querySelector('#inputNumber')
     if (inputNumber.value == '') {
         errormessage.classList.remove('hide')
     } else if (Number(inputNumber.value) == randomNumber) {
-        screen1.classList.add('hide')
-        screen2.classList.remove('hide')
-
+        toggleScreen()
         document.querySelector(
             '.screen2 h2'
         ).innerText = `Acertou em ${xAttempts} tentativas`
-        console.log(xAttempts)
+        // console.log(xAttempts)
+    } else if (Number(inputNumber.value) > 10 || inputNumber.value < 0) {
+        document.querySelector('.errormessage').innerText = `Digite um número de 0 a 10`      
+        errormessage.classList.remove('hide')
     } else if (
         Number(inputNumber.value) == randomNumber + 1 ||
         Number(inputNumber.value) == randomNumber - 1
@@ -46,15 +50,26 @@ function handleTryClick(event) {
         errormessage.classList.add('hide')
         showPopup('Error')
     }
-    inputNumber.value = ''
     xAttempts++
+    inputNumber.value = ''
+
 }
 
-//Try reset application
-btnTry.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click', function () {
-    screen1.classList.remove('hide')
-    screen2.classList.add('hide')
+function handleResetClick() {
+    toggleScreen()
+
     window.location.reload()
     xAttempts = 1
-})
+}
+
+function toggleScreen() {
+    screen1.classList.toggle('hide')
+    screen2.classList.toggle('hide')
+}
+
+function resetKey(e) {
+    if (e.key == 'Enter' && screen1.classList.contains('hide')) {
+        handleResetClick()
+        console.log(e.key)
+    }
+}
